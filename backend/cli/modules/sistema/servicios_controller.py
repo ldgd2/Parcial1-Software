3906bc233d497d5c984055443671ad0db00d7188
Líder <ldgd290@gmail.com>
2026-09-19 19:09:20 -0400

    fix sockets

--- backend/cli/modules/sistema/servicios_controller.py
+++ backend/cli/modules/sistema/servicios_controller.py
@@ -107,6 +107,9 @@ WantedBy=multi-user.target
 
     location / {
         proxy_pass http://localhost:8000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
     }
@@ -139,6 +142,9 @@ server {{
 
     location / {{
         proxy_pass http://localhost:8000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
     }}
