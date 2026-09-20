48f2044b5cb0e62689a370eef13079c751aa2642
Líder <ldgd290@gmail.com>
2026-09-20 11:31:39 -0400

    fix CORS using allow_origin_regex to support real domains

--- backend/main.py
+++ backend/main.py
@@ -65,9 +65,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:3000",
-        "https://diagramador.example.com"
+        "http://localhost:3000"
     ],
+    allow_origin_regex=r"https://diagramador\..*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
